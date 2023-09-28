f44d3fa7f3f89906e133a291b9e3dfd4eba4d580
Orysia Snitko <orsnitko@gmail.com>
2023-09-28 23:54:26 +0300

    commit

---
+++ lab_1_SE/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+class Program
+{
+    static uint FibonacciNum(uint n)
+    {
+        return (n == 0 || n == 1)
+            ? 1
+            : FibonacciNum(n - 1) + FibonacciNum(n - 2);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("N = ");
+        var n = Convert.ToUInt32(Console.ReadLine());
+        Console.WriteLine($"{n} перших чисел Фiбоначчi");
+        for (uint i = 0; i < n; i++)
+        {
+            var separator = i + 1 == n ? string.Empty : ", ";
+            Console.Write($"{FibonacciNum(i)}{separator}");
+        }
+        Console.ReadLine();
+    }
+}
